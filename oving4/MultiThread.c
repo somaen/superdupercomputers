32d5eaeb8051f45f5fe5ba1ac720b8f5b697156c
Einar Johan T. Sømåen <einarjohants@gmail.com>
2013-02-24 18:09:41 +0100

    Add MultiThreading, and make test
    Yes, yes, I know "and" is a verboten word in commit-messages
    so, read it as && ok?

--- oving4/MultiThread.c
+++ oving4/MultiThread.c
@@ -1,19 +1,29 @@
+#include <omp.h>
 #include <math.h>
 #include <stdio.h>
 #include "PrecisionTimer.h"
 
 int main(
-	//int argc, char ** argv
+    //int argc, char ** argv
 ) {
 	struct Precision_Timer *pt = calloc(1, sizeof(struct Precision_Timer));
-	PT_start(pt);
 	size_t limit = 29;
 	printf("Limit is %zu\n", limit);
 	double *Vector = calloc(1 << limit, sizeof(double));
+	Vector--;
 	// Calculate the vector
-	for (int i = 0, j = 1 ; i < (1 << limit) + 0 ; i++, j++) {
-		Vector[i] = 1. / j / j;
+	int tid;
+	#pragma omp parallel private(tid)
+	{
+		tid = omp_get_thread_num();
+		printf("Hi, I'm a thread, What is love? It is %d!!!!!!!\n", tid);
 	}
+	PT_start(pt);
+	#pragma omp parallel for schedule(static)
+	for (int i = 1 ; i < (1 << limit) + 0 ; i++) {
+		Vector[i] = 1. / i / i;
+	}
+	Vector++;
 	PT_stop(pt);
 	PT_diffTime(pt);
 	char *elapsedTime = calloc(1000, sizeof(char));
@@ -22,10 +32,12 @@ int main(
 	for (size_t k = 4 ; k <= limit ; k++) {
 		PT_start(pt);
 		double acc = 0;
+		#pragma omp parallel for schedule(static) \
+reduction(+:acc)
 		for (size_t i = 0; i < (1 << k) ; i++) {
 			acc += Vector[i];
 		}
-		printf("Sum of Vector is: %lf K = %zu\n", acc, k);
+		printf("Sum of Vector is: %lf K = %zi\n", acc, k);
 		printf("Error is: %e\n", acc - M_PI * M_PI / 6);
 		PT_stop(pt);
 		PT_diffTime(pt);
