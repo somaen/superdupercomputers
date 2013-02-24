84362100f02038c11c37d13ec17e8ea308bea3e3
Torje Hoås Digernes <torjehoa@pvv.org>
2013-02-24 18:24:21 +0100

    Somethingsomething

--- oving4/SingleThread.c
+++ oving4/SingleThread.c
@@ -2,31 +2,30 @@
 #include <stdio.h>
 #include "PrecisionTimer.h"
 
-int main(
-	//int argc, char ** argv
-) {
-	struct Precision_Timer *pt = calloc(1, sizeof(struct Precision_Timer));
+int main() {
+	char *elapsedTime = calloc(1000, sizeof(char));
+	struct Precision_Timer *pt = calloc(1,
+			sizeof(struct Precision_Timer));
 	PT_start(pt);
-	size_t limit = 29;
+	size_t limit = 14;
 	printf("Limit is %zu\n", limit);
 	double *Vector = calloc(1 << limit, sizeof(double));
-	// Calculate the vector
-	for (int i = 0, j = 1 ; i < (1 << limit) + 0 ; i++, j++) {
+	size_t ulim = 1 << limit;
+	for (size_t i = 0, j = 1 ; i < ulim ; i++, j++) {
 		Vector[i] = 1. / j / j;
 	}
 	PT_stop(pt);
 	PT_diffTime(pt);
-	char *elapsedTime = calloc(1000, sizeof(char));
 	print_timeval(pt, elapsedTime);
 	printf("%s\n", elapsedTime);
 	for (size_t k = 4 ; k <= limit ; k++) {
 		PT_start(pt);
 		double acc = 0;
-		for (size_t i = 0; i < (1 << k) ; i++) {
+		ulim = 1 << k;
+		for (size_t i = 0; i < ulim ; i++) {
 			acc += Vector[i];
 		}
-		printf("Sum of Vector is: %lf K = %zu\n", acc, k);
-		printf("Error is: %e\n", acc - M_PI * M_PI / 6);
+		printf("Sum - pi²/6: %e\n", acc - M_PI*M_PI/6);
 		PT_stop(pt);
 		PT_diffTime(pt);
 		print_timeval(pt, elapsedTime);
