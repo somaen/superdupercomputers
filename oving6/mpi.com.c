c0b897be4b745bed5809898845fb2708d72658f7
Torje Hoås Digernes <torjehoa@pvv.ntnu.no>
2013-04-06 15:23:33 +0100

    I have done something!!!

--- oving6/mpi.com.c
+++ oving6/mpi.com.c
@@ -1,20 +1,21 @@
 #include "mpi.com.h"
 
-struct mpi_com  mpi_com_Init(struct mpi_comm uplink, int *argc, char ***argv) {
+void mpi_com_Init(struct mpi_com *uplink, int *argc, char ***argv) {
 	MPI_Init(argc, argv);
 	int irank, inprocs;
 	MPI_Comm_rank(MPI_COMM_WORLD, &irank);
+	
 	MPI_Comm_size(MPI_COMM_WORLD, &inprocs);
-	uplink.rank = (size_t)irank;
-	uplink.nprocs = (size_t)inprocs;
-	return uplink;
+	uplink->rank = (size_t)irank;
+	uplink->nprocs = (size_t)inprocs;
+	fprintf( stderr,"rank: %zd nprocs: %zd\n", uplink->rank, uplink->nprocs);
 }
 
 void mpi_com_Finalize(void) {
 	MPI_Finalize();
 }
 
-void generateSendCounts(int *sendCounts , size_t length) {
+void generateSendCounts(struct mpi_com uplink, int *sendCounts , size_t length) {
 	for (size_t i = 0; i < (length % uplink.nprocs) ; i ++) {
 		sendCounts[i] = (int)(length / uplink.nprocs + 1);
 	}
@@ -23,12 +24,12 @@ void generateSendCounts(int *sendCounts , size_t length) {
 	}
 }
 
-void generateSendDisplacements(int *sendDisplacements, int *sendCounts) {
+void generateSendDisplacements(struct mpi_com uplink, int *sendDisplacements, int *sendCounts) {
 	for (size_t i = 1; i <  uplink.nprocs ; i++) {
 		sendDisplacements[i] = sendCounts[i] + sendDisplacements[i - 1];
 	}
 }
-void printCountsAndDisplacements(int *sendCounts, int *sendDisplacements) {
+void printCountsAndDisplacements(struct mpi_com uplink, int *sendCounts, int *sendDisplacements) {
 	printf("Senddisplacements: ");
 	for (size_t i =  0 ; i < uplink.nprocs ; i++) {
 		printf("%d ", sendDisplacements[i]);
--- oving6/mpiMatrix.c
+++ oving6/mpiMatrix.c
@@ -7,27 +7,36 @@ struct mpiMatrix{
 	size_t height;
 	size_t width;
 	size_t widthOffset;
-	size_t widhtLocal;
+	size_t widthLocal;
 	struct mpi_com uplink;
 	double *data;
 };
+
 struct mpiMatrix *mpiMatrix_ctor(size_t height, size_t width, struct mpi_com uplink);
 double *getVector(struct mpiMatrix, size_t column);
+void populate( struct mpiMatrix * matrix , struct mpi_com *uplink) {
+	for ( int i = 0 ; i < matrix -> widthLocal*matrix->height ; i++ ){
+		matrix -> data[i] = uplink->rank;
+	}
+}
 
 int main(int argc, char** argv){
 	struct mpi_com uplink;
-	mpi_com_Init(uplink, &argc, &argv);
-	struct mpiMatrix * matrix =mpiMatrix_ctor(100,100, uplink); 
+	//uplink.nprocs = 1;
+	//uplink.rank =0;
+	mpi_com_Init(&uplink, &argc, &argv);
+	struct mpiMatrix * matrix = mpiMatrix_ctor(100,100, uplink); 
+	printf("%p\n", matrix );
 	mpi_com_Finalize();
 	return 0;
 }
 
 struct mpiMatrix *mpiMatrix_ctor(size_t height, size_t width, struct mpi_com uplink){
 	struct mpiMatrix *matrix = calloc(1, sizeof(struct mpiMatrix));
-	size_t widthLocal = width / uplink.nprocs + ((uplink.rank> width%uplink.nprocs) ? 1:0);
+	size_t widthLocal = width / uplink.nprocs + ((uplink.rank > (width%uplink.nprocs)) ? 1:0);
 	size_t offset;
 
-	if ( uplink.rank>= width%uplink.nprocs){
+	if ( uplink.rank>= ( width % uplink.nprocs )){
 		offset = (width%uplink.nprocs) * (width / uplink.nprocs+1);
 		offset += (uplink.nprocs- (width%uplink.nprocs))*(width / uplink.nprocs+1);
 	}
@@ -40,5 +49,6 @@ struct mpiMatrix *mpiMatrix_ctor(size_t height, size_t width, struct mpi_com upl
 	matrix->width = widthLocal;
 	matrix->height = height;
 	matrix->uplink = uplink;
+	populate ( matrix , &uplink);
 	return matrix;
 }
